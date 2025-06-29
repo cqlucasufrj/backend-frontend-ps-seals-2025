cba2ea2f9aa08629030905ac71fda01ef30e85af
cqlucas <162188832+cqlucasufrj@users.noreply.github.com>
2025-06-29 01:31:46 -0300

    swagger para doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API PS Seals 2025')
+    .setDescription('Essa é a API para o PS Seals 2025, aqui estão os endpoints do sistema.')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 443);
 }
 bootstrap();
