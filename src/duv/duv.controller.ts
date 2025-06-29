2f7c162f46aa4fff30e58ac34f58cabe90561054
cqlucas <162188832+cqlucasufrj@users.noreply.github.com>
2025-06-29 01:54:39 -0300

    doc CRUD duv

--- src/duv/duv.controller.ts
+++ src/duv/duv.controller.ts
@@ -16,21 +16,27 @@ export class DuvController {
     return this.duvService.create(createDuvDto);
   }
 
+  @ApiOperation({ summary: 'Obtém todas as DUVs' })
   @Get()
   findAll() {
     return this.duvService.findAll();
   }
 
+  @ApiOperation({ summary: 'Obtém uma DUV pelo ID' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.duvService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Atualiza uma DUV' })
+  @ApiResponse({ status: 200, description: 'DUV atualizada' })
+  @ApiBody({ type: UpdateDUVDto, description: 'Dados para atualizar uma DUV' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDuvDto: UpdateDUVDto) {
     return this.duvService.update(+id, updateDuvDto);
   }
 
+  @ApiOperation({ summary: 'Deleta uma DUV' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.duvService.remove(+id);
