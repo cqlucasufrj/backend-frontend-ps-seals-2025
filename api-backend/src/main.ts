67269899a5c241d361bfcdbae8bc62002db2c609
cqlucas <162188832+cqlucasufrj@users.noreply.github.com>
2025-06-29 15:30:11 -0300

    habilitando cors no back

--- api-backend/src/main.ts
+++ api-backend/src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('API PS Seals 2025')
     .setDescription('Essa é a API para o PS Seals 2025, aqui estão os endpoints do sistema.')
