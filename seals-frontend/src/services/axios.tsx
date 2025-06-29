5113dd089a0f036c3bcb06a2b91cb9943621b7ba
cqlucas <162188832+cqlucasufrj@users.noreply.github.com>
2025-06-29 15:26:05 -0300

    request das DUVs

--- seals-frontend/src/services/axios.tsx
+++ seals-frontend/src/services/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:443',
+  baseURL: 'http://localhost:443', // Mude a URL caso esteja rodando em outra porta
   headers: {
     'Content-Type': 'application/json',
   },
